c1fc24e3c0d53f6ca982bbd143812bb2d1cbf889
App Academy Student <appacademy@Student-Computer-2.local>
2015-12-02 13:17:49 -0800

    First commit

---
+++ ttt.js-gh-pages/skeleton/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  context: __dirname,
+  entry: "./js/main.js",
+  output: {
+    path: "./js",
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  devtool: 'source-maps'
+};
